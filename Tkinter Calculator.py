9a2deea5009749dc0b71c52fd6c614cc21b40250
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2020-11-21 13:26:57 +0000

    Used lambda as passing functions like this did not work

--- Tkinter Calculator.py
+++ Tkinter Calculator.py
@@ -12,29 +12,29 @@ resultBox = Entry(window, width=50, borderwidth = 6)
 resultBox.grid(row=0, column=0, padx = 60, columnspan=4)
 
 # List of Buttons in order
-clearButton = Button(window, text = "Clear", padx=160, pady=15, command = clickButton())			# Set padding to 160 to over 3 columns
-eqButton = Button(window, text = "=", padx=50, pady=15, command = clickButton())
+clearButton = Button(window, text = "Clear", padx=160, pady=15, command = lambda: clickButton(2))			# Set padding to 160 to over 3 columns
+eqButton = Button(window, text = "=", padx=50, pady=15, command = lambda : clickButton())
 
-Button9 = Button(window, text = "9", padx=50, pady=15, command = clickButton())
-Button8 = Button(window, text = "8", padx=50, pady=15, command = clickButton())
-Button7 = Button(window, text = "7", padx=50, pady=15, command = clickButton())
-plusButton = Button(window, text = "+", padx=50, pady=15, command = clickButton())
+Button9 = Button(window, text = "9", padx=50, pady=15, command = lambda: clickButton(9))
+Button8 = Button(window, text = "8", padx=50, pady=15, command = lambda: clickButton(8))
+Button7 = Button(window, text = "7", padx=50, pady=15, command = lambda: clickButton(7))
+plusButton = Button(window, text = "+", padx=50, pady=15, command = lambda: clickButton())
 
-Button6 = Button(window, text = "6", padx=50, pady=15, command = clickButton())
-Button5 = Button(window, text = "5", padx=50, pady=15, command = clickButton())
-Button4 = Button(window, text = "4", padx=50, pady=15, command = clickButton())
-minusButton = Button(window, text = "-", padx=50, pady=15, command = clickButton())
+Button6 = Button(window, text = "6", padx=50, pady=15, command = lambda: clickButton(6))
+Button5 = Button(window, text = "5", padx=50, pady=15, command = lambda: clickButton(5))
+Button4 = Button(window, text = "4", padx=50, pady=15, command = lambda: clickButton(4))
+minusButton = Button(window, text = "-", padx=50, pady=15, command = lambda: clickButton())
 
-Button3 = Button(window, text = "3", padx=50, pady=15, command = clickButton())
-Button2 = Button(window, text = "2", padx=50, pady=15, command = clickButton())
-Button1 = Button(window, text = "1", padx=50, pady=15, command = clickButton())
-multButton = Button(window, text = "x", padx=50, pady=15, command = clickButton())
+Button3 = Button(window, text = "3", padx=50, pady=15, command = lambda: clickButton(3))
+Button2 = Button(window, text = "2", padx=50, pady=15, command = lambda: clickButton(2))
+Button1 = Button(window, text = "1", padx=50, pady=15, command = lambda: clickButton(1))
+multButton = Button(window, text = "x", padx=50, pady=15, command = lambda: clickButton())
 
-Button0 = Button(window, text = "0", padx=170, pady=15, command = clickButton())				# Set padding to 170 to over 3 columns greater than "Clear" as that was a word
-divButton = Button(window, text = "÷", padx=50, pady=15, command = clickButton())
+Button0 = Button(window, text = "0", padx=170, pady=15, command = lambda: clickButton(0))				# Set padding to 170 to over 3 columns greater than "Clear" as that was a word
+divButton = Button(window, text = "÷", padx=50, pady=15, command = lambda: clickButton())
 
 # Lists of Grids
-clearButton.grid(row=1, column=0, columnspan=3)													# Set columnspan to 3 to cover 3 rows and make it allign
+clearButton.grid(row=1, column=0, columnspan=3)			# Set columnspan to 3 to cover 3 rows and make it allign
 eqButton.grid(row=1, column=3)
 
 Button9.grid(row=2, column=0)
@@ -52,7 +52,7 @@ Button2.grid(row=4, column=1)
 Button1.grid(row=4, column=2)
 multButton.grid(row=4, column=3)
 
-Button0.grid(row=5,column=0, columnspan=3)								# Set columnspan to 3 to cover 3 rows and make it allign
+Button0.grid(row=5,column=0, columnspan=3)			# Set columnspan to 3 to cover 3 rows and make it allign
 divButton.grid(row=5, column=3)
 
 window.mainloop()
